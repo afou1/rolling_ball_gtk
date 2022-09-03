28bdf6f9004774bfeb3385bea9c1aabafa9cfb9d
Mous <fteite@yahoo.fr>
2022-09-03 18:16:07 +0200

    [dev] changing the images path to relative path

---
+++ build/CMakeFiles/rolling_ball_gtk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rolling_ball_gtk.dir/game.c.o"
+  "CMakeFiles/rolling_ball_gtk.dir/main.c.o"
+  "rolling_ball_gtk"
+  "rolling_ball_gtk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rolling_ball_gtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
